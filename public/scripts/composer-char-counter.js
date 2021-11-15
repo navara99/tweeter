ae215e81e9f0382a86c01fbf3010e6f0c8490282
Tharsikan <thar.navaratnam@gmail.com>
2021-11-15 21:51:52 +0000

    add char count logic

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+const maxChars = 140;
+
+$(document).ready(function () {
+
+  function tweetInputHandler() {
+    const tweetLength = $(this).val().length;
+    const charsLeftCount = maxChars - tweetLength;
+    const charsLeftDisplay = $(this).next().children(".counter").text(charsLeftCount);
+    if (charsLeftCount < 0) charsLeftDisplay.addClass("invalid");
+    if (charsLeftCount >= 0) charsLeftDisplay.removeClass("invalid");
+  }
+
+  $("#tweet-text").on("input", tweetInputHandler);
+
+
+});
