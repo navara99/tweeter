2beefd940955b690b7ced16d52572fb1828793a4
Tharsikan <thar.navaratnam@gmail.com>
2021-11-16 03:55:09 +0000

    add createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,50 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+
+  const generateIcons = () => {
+    const icons = ["flag", "retweet", "heart"];
+    const iconElems = icons.map((icon) => $("<i>").addClass(`fas fa-${icon}`));
+    return iconElems;
+  }
+
+  const createTweetElement = (tweet) => {
+    const { user, content, created_at } = tweet;
+
+    // Create the header
+    const $image = $("<img>").attr("src", user.avatars);
+    const $name = $("<div>").text(user.name);
+    const $tweeterHandle = $("<div>").text(user.handle);
+    const $header = $("<header>").append($image,$name,$tweeterHandle);
+
+    // Create the text
+    const $text = $("<div>").text(content.text);
+
+    // Create footer
+    const $timeAgo = $("<div>").text(timeago.format(created_at));
+    const $icons = $("<div>").html(generateIcons());
+    const $footer = $("<footer>").append($timeAgo, $icons);
+
+    // Create article
+    const article = $("<article>").append($header, $text, $footer).addClass("tweet");
+
+    return article;
+  }
+
+  const tweet = {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1636935928442
+  };
+
+  $(".container").append(createTweetElement(tweet));
+
+});
+
